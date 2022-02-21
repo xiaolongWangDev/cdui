8de0003892df50bdcd4be78c505f58a400c8688a
xiaolongwang <xiaolong.wang.dev@gmail.com>
2022-02-20 20:02:09 -0500

    SpendingHeatMapComponent now overrides the click event

--- projects/demo/src/app/components/spending-heat-map/spending-heat-map.component.ts
+++ projects/demo/src/app/components/spending-heat-map/spending-heat-map.component.ts
@@ -7,12 +7,16 @@ import {
 } from "configuration-driven-core";
 import {map} from "rxjs/operators";
 import {SpendingHeatMapConfig} from "./spending-heat-map.config.ts";
+import {Options} from "highcharts";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: "demo-spending-heat-map",
-  template: heat_map_template
+  template: "<span *ngIf='fromCellStr$ | async as fromCellStr'>click event from cell {{fromCellStr}}</span>" + heat_map_template
 })
 export class SpendingHeatMapComponent extends HeatMapComponent<SpendingHeatMapConfig> {
+
+  public fromCellStr$: BehaviorSubject<String> = markAsTracked(new BehaviorSubject<String>(null), "spending_heat_map_from_cell_str");
 
   constructor(obsService: DynamicObservableOrchestrationService,
               changeDetectionRef: ChangeDetectorRef) {
@@ -21,13 +25,23 @@ export class SpendingHeatMapComponent extends HeatMapComponent<SpendingHeatMapCo
 
   protected readyToConsumeObservables(): void {
     super.readyToConsumeObservables();
+    const thisComponent = this;
     this.options$ = markAsTracked(this.options$.pipe(map((options) => {
       return {
         ...options,
-        title: {
-          text: "overridden"
-        }
-      };
+        plotOptions: {
+          series: {
+            cursor: 'pointer',
+            point: {
+              events: {
+                click: function () {
+                  thisComponent.fromCellStr$.next(`(${this.x}, ${this.y})`);
+                }
+              }
+            }
+          }
+        },
+      } as Options;
     })), "spending_heat_map_options");
   }
 }
