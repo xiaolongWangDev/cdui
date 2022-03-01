440ffb31c7f6142d719690399c52450b2a186996
xiaolongwang <xiaolong.wang.dev@gmail.com>
2022-02-28 22:29:01 -0500

    add tab page text

--- projects/demo/src/app/pages/demo-tab-page.component.ts
+++ projects/demo/src/app/pages/demo-tab-page.component.ts
@@ -1,9 +1,6 @@
 import {Component} from '@angular/core';
 import {PlaceholderConfig} from "../components/placeholder/placeholder.config";
 import {PageConfiguration} from "../components/page/page.config";
-import {StoreConfiguration} from "configuration-driven-core";
-import {DropdownConfiguration} from "../components/dropdown/dropdown.config";
-import {RowConfiguration} from "../components/row/row.config";
 import {AlertConfiguration} from "../components/alert/alert.config";
 import {TabConfiguration} from "../components/tab/tab.config";
 
@@ -11,19 +8,13 @@ import {TabConfiguration} from "../components/tab/tab.config";
   template: `
     <demo-toggle>
       <ng-template demo-toggle-target>
-        <demo-tab [config]="config"></demo-tab>
+        <demo-page [config]="config"></demo-page>
       </ng-template>
     </demo-toggle>
   `
 })
 export class DemoTabPageComponent {
-  config = new TabConfiguration({
-    tabLabels: ["Tab X", "Tab Y", "Tab Z"],
-    components: [
-      new PlaceholderConfig({text: "Foo"}),
-      new PlaceholderConfig({text: "Bar"}),
-      new PlaceholderConfig({text: "FooBar"})]
-  });
+  config =demo_tab_conf;
 }
 
 const demo_tab_conf = new PageConfiguration({
@@ -32,9 +23,15 @@ const demo_tab_conf = new PageConfiguration({
     new AlertConfiguration({
       type: "success",
       htmlContent: `
-
-              `
-    }), ]
+        <p>Based on ng-bootstrap nav component.</p>
+        <p>Each tab content should be a dynamic CD component. In this case, they are PlaceholderComponent.</p>`
+    }), new TabConfiguration({
+      tabLabels: ["Tab X", "Tab Y", "Tab Z"],
+      components: [
+        new PlaceholderConfig({text: "Foo"}),
+        new PlaceholderConfig({text: "Bar"}),
+        new PlaceholderConfig({text: "FooBar"})]
+    })]
 });
 
 
